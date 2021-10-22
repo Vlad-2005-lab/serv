11c45dcb472301038ede48bf50d4e923aa11fa54
Vlad <anikanovvlad@yandex.ru>
2021-10-22 20:23:19 +0300

    heroku

--- push.py
+++ push.py
@@ -1,5 +1,5 @@
 f = open("data.txt", "w")
 f1 = open("requirements.txt", "r")
 for i in f1.read().split("\n"):
-    f.write("==".join(i.split()))
+    f.write("==".join(i.split("==")[:2]))
     f.write("\n")
